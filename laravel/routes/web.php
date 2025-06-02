aee514744e607eba490f17a69612cc583c475b09
Embun Nawang Sari <embunnawangsari@gmail.com>
2025-06-02 18:57:31 +0700

    add page login etc

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/forgotpassword_fillemail', function () {
+    return view('forgotpassword_fillemail');
+});
+Route::get('/forgotpassword_fillotp', function () {
+    return view('forgotpassword_fillotp');
+});
+Route::get('/forgotpassword_fillpassword', function () {
+    return view('forgotpassword_fillpassword');
+});
